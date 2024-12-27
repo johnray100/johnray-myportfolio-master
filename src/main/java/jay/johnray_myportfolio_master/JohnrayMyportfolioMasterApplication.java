30355c7fda6af0ee7951405a4096c4bcf63274e3
johnray100 <jfornias10@gmail.com>
2024-12-27 10:27:53 +0800

    first

---
+++ src/main/java/jay/johnray_myportfolio_master/JohnrayMyportfolioMasterApplication.java
@@ -0,0 +1,13 @@
+package jay.johnray_myportfolio_master;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JohnrayMyportfolioMasterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JohnrayMyportfolioMasterApplication.class, args);
+	}
+
+}
